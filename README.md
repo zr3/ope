edf6a03abe612087790dc2be34c9a01af55b123e
zr3 <zak.reynolds+github@zakjr.com>
2023-07-07 01:06:36 -0700

    more basic messy features

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # ope
+
 a CLI tool for reminding yourself of all your flaws
+
+## usage
+
+call it with a quick note to save the note:
+
+```
+$ ope i "hacked in a quick fix to get the frontend running"
+```
+
+view the notes:
+
+```
+$ ope look
+2023-07-06T23:32:07.414061461Z [HACK] hacked in a quick fix to get the frontend running
+```
+
+call it with 'will' to save the note as a todo instead:
+
+```
+$ ope will "add support for easier API calls"
+$ ope will "set up libinput-gestures"
+$ ope look
+FIX THIS:
+2023-07-06T23:32:07.414061461Z [HACK] hacked in a quick fix to get the frontend running
+
+DO THIS:
+2023-07-06T23:35:07.414061461Z [TODO] add support for easier API calls
+2023-07-06T23:35:07.414061461Z [TODO] set up libinput-gestures
+```
+
+## installation
+
+clone and build. packaging is WIP
+
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,113 @@
 use anyhow::{Context, Result};
-use clap::Parser;
+use clap::{arg, command, Command};
+use directories::ProjectDirs;
 use simplelog::{ConfigBuilder, LevelFilter, WriteLogger};
 use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::PathBuf;
+use time::format_description::well_known::Rfc3339;
+use time::OffsetDateTime;
 
 // log quick snappy notes to keep track of side thoughts
-#[derive(Parser)]
-struct Cli {
-    note: String,
+struct Config {
+    hack_path: PathBuf,
+    todo_path: PathBuf,
 }
 
 fn main() -> Result<()> {
-    let args = Cli::parse();
-    let mut log_filepath = home::home_dir().unwrap();
-    log_filepath.push(".ope");
+    // instantiate config
+    let proj_dirs = ProjectDirs::from("codes", "gnaw", "ope").expect("no project dir exists");
+    std::fs::create_dir_all(proj_dirs.data_dir())?;
+    let hack_file = proj_dirs.data_dir().join("hack.log");
+    let todo_file = proj_dirs.data_dir().join("todo.log");
+    let config = Config {
+        hack_path: hack_file,
+        todo_path: todo_file,
+    };
+
+    // set up logging
+    let applog_file = proj_dirs.data_dir().join("ope.log");
     let log_config = ConfigBuilder::new().set_time_format_rfc3339().build();
     let log_file = OpenOptions::new()
         .create(true)
         .append(true)
-        .open(log_filepath)
+        .open(applog_file)
         .with_context(|| format!("could not open file `{}`", "test.log"))?;
     let _ = WriteLogger::init(LevelFilter::Info, log_config, log_file);
-    log::info!("[NOTE] {}", args.note);
+
+    // parse CLI arguments
+    let matches = command!()
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("i")
+                .about("WHAT DID YOU DO??")
+                .arg(arg!([MISTAKE])),
+        )
+        .subcommand(
+            Command::new("will")
+                .about("what will you do?")
+                .arg(arg!([HOPESANDDREAMS])),
+        )
+        .subcommand(Command::new("look").about("see your lists"))
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("i", sub_matches)) => run_hack(
+            config,
+            Note {
+                value: sub_matches
+                    .get_one::<String>("MISTAKE")
+                    .expect("need a mistake")
+                    .to_string(),
+            },
+        ),
+        Some(("will", sub_matches)) => run_todo(
+            config,
+            Note {
+                value: sub_matches
+                    .get_one::<String>("HOPESANDDREAMS")
+                    .expect("need a todo")
+                    .to_string(),
+            },
+        ),
+        Some(("look", _sub_matches)) => run_look(config),
+        _ => unreachable!("could not find a valid subcommand"),
+    }
+}
+
+struct Note {
+    value: String,
+}
+
+fn run_hack(config: Config, note: Note) -> Result<()> {
+    write_note(config.hack_path.as_path(), String::from("HACK"), note)
+}
+
+fn run_todo(config: Config, note: Note) -> Result<()> {
+    write_note(config.todo_path.as_path(), String::from("TODO"), note)
+}
+
+fn write_note(path: &std::path::Path, note_type: String, note: Note) -> Result<()> {
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path)
+        .with_context(|| format!("could not open file `{}`", path.display()))?;
+    let timestamp = OffsetDateTime::now_utc().format(&Rfc3339)?;
+    writeln!(&mut file, "{}: [{}] {}", timestamp, note_type, note.value)?;
+    Ok(())
+}
+
+fn run_look(config: Config) -> Result<()> {
+    let hacks: String =
+        String::from_utf8_lossy(&std::fs::read(config.hack_path.as_path())?).parse()?;
+    println!("FIX THIS:");
+    println!("{}", hacks);
+    let todos: String =
+        String::from_utf8_lossy(&std::fs::read(config.todo_path.as_path())?).parse()?;
+    println!("DO THIS:");
+    println!("{}", todos);
     Ok(())
 }
